c735273a8dc4dfafc056b0399192e9c328129156
SAMVELMAR <samvel.margaryanxr@gmail.com>
2020-09-23 23:21:16 +0400

    price_With_Change_Currency

--- src/Components/Price.js
+++ src/Components/Price.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
 
 class Price extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            price: props.data,
+            rate: 487
+        }
+    }
+
+    changeCurrency = () =>{
+        let { price, rate } = this.state
+        let currency = price[price.length - 1]
+
+        if (currency === '$') {
+            price = parseFloat(price) * rate + '֏'
+        } else if (currency === '֏') {
+            price = parseFloat(price) / rate + '$'
+        }
+
+
+        this.setState({
+            price: price
+        })
+    }
     
     render(){
-        let {data} = this.props
+        let {price} = this.state
         return(
-                <p>Price: {data}</p>
+                <p>Price: {price}
+                <input
+                 type ='button'
+                 value ='Change currency'
+                 onClick = {this.changeCurrency}
+                 />
+                </p>
         )
     }
 
